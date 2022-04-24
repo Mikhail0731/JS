15094e930e785d6a704dab240b454770e30342fc
MikhailGutsaluyk <mikhail0731@gmail.com>
2022-04-23 20:07:19 -0600

    add js.product

---
+++ products.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    id: 0,
+    name: "Artisan® Series 5 Quart Tilt-Head Stand Mixer",
+    price: 199.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "/images/Artisan® Series 5 Quart Tilt-Head Stand Mixer.jpg",
+  },
+  {
+    id: 1,
+    name: "Classic™ Series 4.5 Quart Tilt-Head Stand Mixer",
+    price: 249.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Classic™ Series 4.5 Quart Tilt-Head Stand Mixer.jpg",
+  },
+  {
+    id: 2,
+    name: "Professional 5™ Plus Series 5 Quart Bowl-Lift Stand Mixer",
+    price: 299.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Professional 5™ Plus Series 5 Quart Bowl-Lift Stand Mixer.jpg",
+  },
+  {
+    id: 3,
+    name: "Smeg Blender Retro Style Aquamarine",
+    price: 139.99,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Smeg Blender Retro Style Aquamarine.jpg",
+  },
+  {
+    id: 4,
+    name: "Smeg Blender Retro Style Creme",
+    price: 139.99,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Smeg Blender Retro Style Creme.jpg",
+  },
+  {
+    id: 5,
+    name: "Smeg Blender Retro Style Red",
+    price: 139.99,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Smeg Blender Retro Style Red.jpg",
+  },
+  {
+    id: 6,
+    name: "X1 iperEspresso Anniversary 1935 Machine",
+    price: 499.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/X1 iperEspresso Anniversary 1935 Machine.png",
+  },
+  {
+    id: 7,
+    name: "Espresso & Coffee Machine - Y3.3 iperEspresso",
+    price: 299.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Espresso & Coffee Machine - Y3.3 iperEspresso.png",
+  },
+  {
+    id: 8,
+    name: "X7.1 iperEspresso Machine",
+    price: 399.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/X7.1 iperEspresso Machine.png",
+  },
+];
