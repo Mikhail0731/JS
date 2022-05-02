c04f997e3e43501fa16931d9411962ad6762b58e
MikhailGutsaluyk <mikhail0731@gmail.com>
2022-05-01 20:24:58 -0600

    added env

--- mern/BackEnd/server.js
+++ mern/BackEnd/server.js
@@ -2,17 +2,26 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://mikhail0731:Quartett007@cluster0.ts5vu.mongodb.net/myFirstStore?retryWrites=true&w=majority"
-)
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
 .then(()=>console.log("DBConnection Successful!"))
 .catch((err) => {
     console.log(err);
 });
-app.listen(3300, ()=> {
-    console.log('Server is running!')
+
+app.get("/api", ()=>{
+    console.log("Helloooo test")
 })
+
+app.listen(process.env.PORT || 3300, function () {
+        console.log('Server is running!');
+    })
 
 /*app.get('/', (req, res) => {
     res.sendFile('/Users/Михаил/Desktop/IT/JS' + '/Cart.html')
 })*/
-console.log('global3')
+
