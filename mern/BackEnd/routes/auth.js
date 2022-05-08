3b8677b5bf3d869d080165e0b84abb4503d1d592
MikhailGutsaluyk <mikhail0731@gmail.com>
2022-05-07 20:58:29 -0600

    update user

--- mern/BackEnd/routes/auth.js
+++ mern/BackEnd/routes/auth.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User")
-
+const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken");
 //Register
 router.post("/register", async (req,res)=>{
     const newUser = new User({
-        username:req.body.username,
-        email:req.body.email,
-       password:req.body.password,
+        username: req.body.username,
+        email: req.body.email,
+       password: CryptoJS.AES.encrypt(
+           req.body.password,
+           process.env.PASS_SEC
+
+       ).toString(),
     });
 
 try {
@@ -16,4 +21,32 @@ try {
     res.status(500).json(err);
 }
 });
+
+//LOGIN
+router.post("/login", async (req,res)=>{
+    try {
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(401).json("Wrong username!")
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password, 
+            process.env.PASS_SEC)
+            const origPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+origPassword !==req.body.password && res.status(401).json("Wrong password!");
+
+const accessToken = jwt.sign({
+    id:user._id, 
+    isAdmin: user._isAdmin
+}, process.env.JWT_SEC,
+{expiresIn:"3d"}
+);
+
+const {password, ...others} = user._doc;
+
+res.status(200).json({...others, accessToken});
+    } catch (err) {
+        res.status(500).json(err);
+    }  
+})
+
+
 module.exports = router;
--- mern/BackEnd/routes/user.js
+++ mern/BackEnd/routes/user.js
@@ -1,7 +1,31 @@
+const { verify } = require("jsonwebtoken");
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAutorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
+//UPDATE
 
-module.exports = router
+router.put("/:id", verifyTokenAndAutorization, (req,res)=>{
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString()
+    }
+
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        },{new:true}
+        );
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
 
 
 
